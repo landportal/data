a56eb23722f9f9b8e0d21c1bdce492fa004760d3
Carlos Tejo <carlos.tejo@landportal.info>
2017-02-03 16:21:26 +0000

    updated python script to obtain descriptive statistics about the indicators

--- landbook/scratch/indicators-stats-review.py
+++ landbook/scratch/indicators-stats-review.py
@@ -1,43 +1,81 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 from cmath import sqrt
+import datetime
 
 def statsReview(indicator):
+    date = now = datetime.datetime.now().strftime("%Y-%m-%d")
+
     sparql = SPARQLWrapper("https://landportal.info/sparql")
     sparql.setQuery("""
     PREFIX cex: <http://purl.org/weso/ontology/computex#>
-    SELECT (MIN(?value) AS ?minvalue)  (MAX(?value) AS ?maxvalue) (?mean) (?count) (SUM(((xsd:float(?value - ?mean))*(xsd:float(?value - ?mean))))/(?count - 1) AS ?variance)
-    FROM <http://data.landportal.info>
-    WHERE {
-    ?obs cex:ref-indicator <"""+indicator+"""> ;
-         cex:value ?value .
-    {  
-        SELECT (AVG(?value) as ?mean) (COUNT(?value) as ?count)
-        FROM <http://data.landportal.info>
-        WHERE {
-         ?obs cex:ref-indicator <"""+indicator+"""> ;
-           cex:value ?value .
-        }
-    }
-    }
+
+	SELECT 
+	(year(min(?dateTime)) AS ?minYear)
+	(year(max(?dateTime)) AS ?maxYear)
+	(?count)
+	(COUNT(DISTINCT(year(?dateTime))) AS ?nYears)
+	(COUNT(DISTINCT ?country) AS ?nCountryWithValue)
+	(MIN(?value) AS ?minValue)
+	(MAX(?value) AS ?maxValue)
+	(?mean)
+	(SUM(((xsd:float(?value - ?mean))*(xsd:float(?value - ?mean))))/(?count - 1) AS ?variance)
+	
+	FROM <http://data.landportal.info>
+	
+	WHERE {
+	?obs cex:ref-indicator <"""+indicator+"""> ;
+		 cex:value ?value ;
+		 cex:ref-area ?country ;
+		 cex:ref-time ?time .
+	?time time:hasBeginning ?timeValue .
+	?timeValue time:inXSDDateTime ?dateTime .
+	{  
+		SELECT (AVG(?value) as ?mean) (COUNT(?value) as ?count)
+		FROM <http://data.landportal.info>
+		WHERE {
+		 ?obs cex:ref-indicator <"""+indicator+"""> ;
+		   cex:value ?value .
+		}
+	}
+	}
     """)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     for result in results["results"]["bindings"]:
-        min = result["minvalue"]["value"]
-        max = result["maxvalue"]["value"]
-        mean = result["mean"]["value"]
+        minYear = result["minYear"]["value"]
+        maxYear = result["maxYear"]["value"]
+        #unit = result["unit"]["value"]
         count = result["count"]["value"]
+        nYears = result["nYears"]["value"]
+        nCountryWithValue = result["nCountryWithValue"]["value"]
+        minValue = (result["minValue"]["value"]).replace('.', ',')
+        maxValue = (result["maxValue"]["value"]).replace('.', ',')
+        mean = (result["mean"]["value"]).replace('.', ',')
+        perMissingValue = (str((1-(float(count))/(float(nYears)*float(nCountryWithValue)))*100)).replace('.', ',')
         variance = result["variance"]["value"]
-        
-        print "INDICATOR: "+ indicator
-        print "MIN: "+ min
-        print "MAX: "+ max
+        standardDeviation = (str(sqrt(float(variance)))).replace('.', ',')
+        indicatorID = indicator.replace("http://data.landportal.info/indicator/", "")
+        """
+        print "INDICATOR: "+ indicatorID
+        print "MIN YEAR: "+ minYear
+        print "MAX YEAR: " + maxYear
+        #print "UNIT: " + unit
+        print "COUNT (number observations): "+ count
+        print "NUMBER YEARS: " + nYears
+        print "TOTAL COUNTRIES: " + nCountryWithValue
+        print "MISSING VALUES (%)" + perMissingValue
+        print "MIN VALUE: "+ minValue
+        print "MAX VALUE: "+ maxValue
         print "MEAN: "+ mean
-        print "COUNT: "+ count
-        print "VARIANCE: "+ variance
-        print "STANDARD DEVIATION: "+str(sqrt(float(variance)))
+        print "VARIANCE: "+ variance.replace('.', ',')
+        print "STANDARD DEVIATION: " + standardDeviation
         print "---------------------------------------"
-        
+        """        
+		# CSV: "INDICATOR - MIN - MAX - MEAN - COUNT - VARIANCE - STANDARD DEVIATION"
+		# COUNT = Number of observations
+		# NUMBER YEARS = NUMBER OF YEARS WITH AT LEAST ONE VALUE
+		# NUMBER COUNTRIES = NUMBER OF COUNTRIES WITH AT LEAST ONE VALUE
+        print date + ";" + indicatorID + ";" + minYear + ";" + maxYear + ";" + count + ";" + nYears + ";" + nCountryWithValue + ";" + perMissingValue + ";" + minValue + ";" + maxValue + ";" + mean + ";" + variance.replace('.', ',') + ";" + standardDeviation
 
 
 def getIndicators():
@@ -47,7 +85,7 @@ def getIndicators():
     PREFIX cex: <http://purl.org/weso/ontology/computex#>
     
     SELECT DISTINCT ?indicatorURL
-    FROM <http://data.landportal.info>
+    FROM <http://old-data.landportal.info>
     WHERE {
        ?indicatorURL a cex:Indicator . # ?obs cex:ref-indicator ?indicatorURL .
     } ORDER BY ?indicatorURL
@@ -63,9 +101,13 @@ def getIndicators():
         #print("indicatorURL: " + indicatorURL)
         indicators.append(indicatorURL)
     return indicators
-        
+
+
+# main	
+#header 
+print "date ; indicator ID; minYear ; maxYear; number observations ; number years ; total countries ; missing values ; min value ; max value ; mean ; variance ; standard deviation"
+
 indicators = getIndicators()
 
-# main
 for indicator in indicators :
     statsReview(indicator)
