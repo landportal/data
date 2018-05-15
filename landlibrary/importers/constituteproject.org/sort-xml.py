2ce7f29fd8a1e350b87395922dec04ec43fb3452
Carlos Tejo <carlos.tejo@landportal.info>
2018-05-15 13:06:14 +0200

    Added sorting script

---
+++ landlibrary/importers/constituteproject.org/sort-xml.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+
+filename = "20170306 - RDF generated from constituteproject.org.rdf"
+tree = ET.parse(filename)
+ET.register_namespace('dct',"http://purl.org/dc/terms/")
+ET.register_namespace('bibo',"http://purl.org/ontology/bibo/")
+    
+# this element holds the phonebook entries
+container = tree.getroot()
+data = []
+for elem in container:
+    key = elem.findtext("{http://purl.org/dc/elements/1.1/}coverage")
+    data.append((key, elem))
+
+data.sort()
+
+# insert the last item from each tuple
+container[:] = [item[-1] for item in data]
+
+tree.write("ordered-"+filaname)
