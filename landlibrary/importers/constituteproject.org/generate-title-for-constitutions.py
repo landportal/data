829a9acb59ce8f513b528ba454a3a3a8c7a5770d
Carlos Tejo <carlos.tejo@landportal.info>
2018-05-15 13:03:22 +0200

    updated files and scripts with 20180515 information.Added generated RDF files

--- landlibrary/importers/constituteproject.org/generate-title-for-constitutions.py
+++ landlibrary/importers/constituteproject.org/generate-title-for-constitutions.py
@@ -1,20 +1,28 @@
 import urllib, json
 from sets import Set
+import codecs
 
 #url = "https://www.constituteproject.org/service/constitutions"
 #response = urllib.urlopen(url) 
 #html=response.read() 
 #data = json.loads(html)
 
-with open('constitutions') as data_file:    
+with codecs.open('constitutions.json', "r", "utf-8") as data_file:    
     data = json.load(data_file)
-i = 0
-for constitution in data:
-	i = i + 1
-	id = constitution['id']
-	title = constitution['title']
-	print "<http://www.constituteproject.org/constitution/%s> dct:title \"Constitution of %s\" ." %(id,title)
-	#print "%i - Constitution #%s" %(i,id)
 
-print "Total %d" %i
-print "**********************************"
+prefix = u"@prefix dct: <http://purl.org/dc/terms/> ."
+with codecs.open('constitutions-title.ttl',"w", "utf-8") as ttl_file:
+    ttl_file.write(prefix+"\n")    
+    i = 0
+    for constitution in data:
+    	i = i + 1
+    	id = constitution['id']
+    	title = constitution['title']
+    	line = u"<http://www.constituteproject.org/constitution/%s> dct:title \"Constitution of %s\" ." %(id,title)
+        ttl_file.write(line+u"\n")
+        print line
+    	#print "%i - Constitution #%s" %(i,id)
+    print "Total %d" %i
+    print "**********************************"
+    ttl_file.close()
+
